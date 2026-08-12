8b7ac1bdc0f6f338699c53b7c7f412a2e4587338
Turhan Can Kargın <turhancan.kargin@gmail.com>
2026-08-12 17:23:39 +0200

    feat: add SLURM scripts for probe and finetune tasks and update gitignore to include dataset directory

---
+++ finetune.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -p rtx4090_batch
+#SBATCH --gpus=1
+#SBATCH --qos=batch
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=64G
+#SBATCH --ntasks=1
+#SBATCH --job-name=finetune_reid
+#SBATCH --time=24:00:00
+#SBATCH --output=logs/%x_%j.log
+
+nvidia-smi -L
+
+conda init bash
+source /shared/results/common/kargin/tck_miniconda3/etc/profile.d/conda.sh
+conda activate ex-reid
+
+python train/finetune.py
---
+++ probe.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -p dgxa100
+#SBATCH --gpus=1
+#SBATCH --qos=big
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=64G
+#SBATCH --ntasks=1
+#SBATCH --job-name=probe_reid
+#SBATCH --time=24:00:00
+#SBATCH --output=logs/%x_%j.log
+
+nvidia-smi -L
+
+conda init bash
+source /shared/results/common/kargin/tck_miniconda3/etc/profile.d/conda.sh
+conda activate ex-reid
+
+python train/probe.py
